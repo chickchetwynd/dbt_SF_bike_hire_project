092af7bc335c36d6ff59af12c0a1a909b12d27c5
Chick <chetwynd.chick@gmail.com>
2023-05-24 16:46:24 -0700

    Adding models for Power BI dashboard.

--- models/data_mart/Power BI Dashboard/rain_vs_user_type.sql
+++ models/data_mart/Power BI Dashboard/rain_vs_user_type.sql
@@ -4,6 +4,6 @@ SELECT
   user_type,
   COUNT(ride_id) AS number_of_rides
 
-FROM {{ REF("sf_bike_hire_and_weather") }}
+FROM {{ ref("sf_bike_hire_and_weather") }}
 
 GROUP BY rainfall_category, user_type
